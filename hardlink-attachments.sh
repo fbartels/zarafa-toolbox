b5d05eec1fe680258d378bd315eaa010cc56573d
Felix Bartels <felix@host-consultants.de>
2012-05-04 07:00:45 +0200

    Whats the fun in reducing attachments, if you don't know from where you start?

--- hardlink-attachments.sh
+++ hardlink-attachments.sh
@@ -2,5 +2,9 @@
 ## Searches the Zarafa attachments directory and replaces duplicate files 
 ## with hardlinks
 ## Requires the Debian packages "hardlink" to be installed
+
+echo "Current size of attachments"
+du -hs /var/lib/zarafa
+
 ## remove the "n" to actually do anything.
 hardlink -tnv /var/lib/zarafa/ -x index
